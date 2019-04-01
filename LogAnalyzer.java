d24df753100bac766ff0e885a42f388a9d0bce69
FrostHanzo <47114061+FrostHanzo@users.noreply.github.com>
2019-04-01 08:16:55 -0400

    7.12-7.18

--- LogAnalyzer.java
+++ LogAnalyzer.java
@@ -55,4 +55,71 @@ public class LogAnalyzer
     {
         reader.printData();
     }
+    public int numberOfAccesses()
+    {
+            int total = 0;
+            // Add the value in each element of hourCounts
+            // to total.
+            for(int hour = 0; hour < hourCounts.length; hour++) {
+            total = total + hourCounts[hour];
+            }
+            return total;
+    }
+      
+    public int busiestHour()
+    {
+            int numberOfAccessesAtBusiest = 0;
+            int busiestHour = 0;
+            int index = 0;
+            
+            while(index < hourCounts.length - 1){
+                if (numberOfAccessesAtBusiest < hourCounts[index]){
+                    busiestHour = index;
+                    numberOfAccessesAtBusiest = hourCounts[index];
+                    index++;
+                }
+                else {
+                    index++;
+                }
+            }
+            return busiestHour;
+    }
+    
+    public int quietestHour()
+    {
+            int numberOfAccessesAtQuietest = 9999;
+            int quietestHour = 0;
+            int index = 0;
+            
+            while(index < hourCounts.length - 1){
+                if (numberOfAccessesAtQuietest > hourCounts[index]){
+                    quietestHour = index;
+                    numberOfAccessesAtQuietest = hourCounts[index];
+                    index++;
+                }
+                else {
+                    index++;
+                }
+            }
+            return quietestHour;
+    }
+    
+    public int twobusiestHour()
+    {
+            int numberOfAccessesAtBusiest = 0;
+            int busiestHour = 0;
+            int index = 0;
+            
+            while(index < hourCounts.length - 1){
+                if (numberOfAccessesAtBusiest < hourCounts[index] + hourCounts[index + 1]){
+                    busiestHour = index;
+                    numberOfAccessesAtBusiest = hourCounts[index] + hourCounts[index + 1];
+                    index++;
+                }
+                else {
+                    index++;
+                }
+            }
+            return busiestHour;
+    }          
 }
